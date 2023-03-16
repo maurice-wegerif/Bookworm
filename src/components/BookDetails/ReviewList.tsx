4fd340ed6ef67c23405c87e1b00717bb75c058fa
Maurice Wegerif <maurice.wegerif@ntnu.no>
2023-03-16 09:54:05 +0100

    Feature/#36 delete review

--- src/components/BookDetails/ReviewList.tsx
+++ src/components/BookDetails/ReviewList.tsx
@@ -1,4 +1,9 @@
-import { Book } from "../../helpers/types";
+import { Book, Review } from "../../helpers/types";
+import { BsTrash } from "react-icons/Bs";
+import { DataContext } from "../../helpers/DataContext";
+import { useContext } from "react";
+import { doc, getFirestore, updateDoc } from "firebase/firestore";
+import { auth } from "../../firebase";
 
 interface ReviewListProps {
   book: Book;
@@ -8,18 +13,41 @@ const printWorms = (amount: number) => {
   return "🐛".repeat(amount);
 };
 
+const deleteReview = async (review: Review, book: Book) => {
+  if (book.reviews.includes(review)) {
+    book.reviews = book.reviews.filter((tempReview) => tempReview !== review);
+    const db = getFirestore();
+    await updateDoc(doc(db, "books", book.id), {
+      reviews: book.reviews,
+    });
+    window.location.reload();
+  }
+};
+
 export const ReviewList = ({ book }: ReviewListProps) => {
+  const { isAdmin } = useContext(DataContext);
   return (
     <div className="flex flex-wrap max-w-3xl m-auto gap-4 py-2">
       {book.reviews.map((review) => (
-        <div className="bg-surface w-full p-4 shadow rounded transition-all cursor-pointer m-auto flex flex-row hover:bg-secondaryBackground">
+        <div
+          key={review.userID}
+          className="bg-background w-full p-4 shadow rounded transition-all cursor-pointer m-auto relative hover:bg-secondaryBackground"
+        >
           <div className="basis-1/2">
-            <h2 className="font-bold text-text">{review.userName}</h2>
+            <div className="flex w-full">
+              <h2 className="font-bold text-text">{review.userName}</h2>
+            </div>
             <span className="text-lightText text-sm">{review.comment}</span>
             <p className="text-lightText text-xl">
               Rating: {printWorms(review.rating)}
             </p>
           </div>
+          {(isAdmin || auth.currentUser?.uid === review.userID) && (
+            <BsTrash
+              className="float-right text-text hover:text-red-500"
+              onClick={() => deleteReview(review, book)}
+            />
+          )}
         </div>
       ))}
     </div>
