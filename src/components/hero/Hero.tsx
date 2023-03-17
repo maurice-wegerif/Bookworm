bec42225a4ab327a6430ae29fea6b983e400e743
Aksel Mellingen <aksel.mellingen@ntnu.no>
2023-03-17 10:53:34 +0100

    Refactor/#39 improve book of day logic

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -10,9 +10,20 @@ export const Hero = () => {
   const fantasyBooks = books.filter((book) => book.genres.includes("fantasy"));
   const fictionBooks = books.filter((book) => book.genres.includes("fiction"));
   const romanBooks = books.filter((book) => book.genres.includes("roman"));
+  //const bookOfDay = books.find((book) => book.title === "1984");
+
+  const today = new Date();
+  const dayOfYear = Math.floor((today.getTime() - new Date(today.getFullYear(), 0, 0).getTime()) / 86400000);
+  const randomIndex = (dayOfYear + 5) % books.length; //Kan evt fjerne +5 når en vil, bare for testing
+  const bookOfDay = books[randomIndex];
+
 
   return (
-    <div className="bg-surface py-24 px-6 sm:py-6 lg:px-8">
+    <div className="bg-surface py-24 px-6 sm:pb-6 sm:pt-2 lg:px-8">
+      {bookOfDay && <HomepageBookOfDay heading="Book of the Day:" book={bookOfDay} /> }
+      <HomepageBookList heading="Best from fantasy" books={fantasyBooks} />
+      <HomepageBookList heading="Best from fiction" books={fictionBooks} />
+      <HomepageBookList heading="Best from roman" books={romanBooks} />
       <div className="flex items-center justify-between">
         <Link to="/TopTenPage" className="text-4xl font-serif text-text">
           Top 10
