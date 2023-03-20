bd9a3cfe7bb78ced8b35e21ef9279f042060438b
Herman Gunnarsen <hermahg@stud.ntnu.no>
2023-03-20 16:27:22 +0100

    UI for Footer, issue 40

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   const links = {
     company: [
@@ -8,54 +10,34 @@ export const Footer = () => {
   };
 
   return (
-    <footer className="bg-surface" aria-labelledby="footer-heading">
+    <footer
+      className="bg-surface font-thin text-text"
+      aria-labelledby="footer-heading"
+    >
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
-        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-          <h1 className="text-4xl">
-            🐛 <span className="text-sm text-text">- worm noises</span>
-          </h1>
-          <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                  💜
-                </h3>
-              </div>
-              <div className="mt-10 md:mt-0">
-                <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                  😬
-                </h3>
-              </div>
-            </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                  😇
-                </h3>
-              </div>
-              <div className="mt-10 md:mt-0">
-                <h3 className="text-sm font-semibold leading-6 text-text">
-                  Bookworm
-                </h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {links.company.map((item) => (
-                    <li key={item.name}>
-                      <a
-                        href={item.href}
-                        className="text-sm leading-6 text-lightText"
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
+      <div className="flex flex-row mx-auto max-w-5xl text-center pt-14 sm:pt-14 lg:pt-14">
+        <div className="basis-1/5">
+          <Link to="/Information"> About us </Link>
         </div>
+        <div className="basis-1/5">
+          <Link to="/Information"> Advertising </Link>
+        </div>
+        <div className="basis-1/5">
+          <Link to="/Information"> Help </Link>
+        </div>
+        <div className="basis-1/5">
+          <Link to="/Information"> Contact</Link>
+        </div>
+        <div className="basis-1/5">Privacy Policy</div>
+      </div>
+      <div className="flex flex-row mx-auto max-w-5xl px-6 py-8 sm:py-8 lg:px-8 lg:py-8 text-center text-2xl">
+        <div className="basis-full">An Independent Company</div>
+      </div>
+
+      <div className="flex flex-row mx-auto max-w-5xl px-6 pb-12 sm:pb-12 lg:px-8 lg:pb-24 text-center text-sm">
+        <div className="basis-full">© 2023, Booklet.com</div>
       </div>
     </footer>
   );
